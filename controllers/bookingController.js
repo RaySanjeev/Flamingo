a71ba4ad4a9e747b790e7f7d84bd4aa6a2a618f4
Sanjeev Ray <sanjeevat1996@gmail.com>
2021-02-18 00:10:34 +0530

    Bug fixes -2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -44,7 +44,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   console.log(`user: ${user}`);
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 
